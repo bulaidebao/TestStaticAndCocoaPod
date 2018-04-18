a4f39e842404c7bdf0a2b5922d56067a1bfe6311
杨 树. 宝 <yangshubao@hualala.com>
2018-04-18 08:49:25 +0800

    This version adds the files of static lib

---
+++ TestStaticLib1/TestStaticLib1.h
@@ -0,0 +1,15 @@
+//
+//  TestStaticLib1.h
+//  TestStaticLib1
+//
+//  Created by apple on 2018/4/18.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestStaticLib1 : NSObject
+
++ (void)hello;
+
+@end
